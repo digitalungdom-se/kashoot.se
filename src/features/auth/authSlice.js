1ac3d877fd64e2a96b3c0418af1f1660ab412ae1
Nautman <douglas@digitalungdom.se>
2019-12-04 23:05:40 +0100

    Added some documentation using JSDoc format

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,6 +4,28 @@ import { createSlice } from '@reduxjs/toolkit';
 // eslint-disable-next-line import/no-unresolved
 import betterFetch from 'utils/betterFetch';
 
+/**
+ * @typedef   {Object} Response
+ * @property  {string}        type      - is either 'success' or 'fail'
+ * @property  {Success}       response  - is only set on successful requests
+ * @property  {Array.<Error>} errors    - is only set on erroneous requests
+ */
+
+/**
+ * @typedef   {Object} Success
+ * @property  {string} _id       - the users mongoID
+ * @property  {string} name      - the name of the user, i.e. 'Douglas Bengtsson'
+ * @property  {string} username
+ * @property  {string} email
+ */
+
+
+/**
+ * @typedef   {Object} Error
+ * @property  {string} msg    - helpful message describing the error
+ * @property  {string} param  - the unsuccessful field
+ */
+
 export const initialState = {
   authorizing: false,
   authorized: false,
@@ -65,21 +87,7 @@ export const { authStart, authSuccess, authFailure } = authSlice.actions;
  * Authorize at kashoot
  * @param {string} url     api route
  * @param {string} cookie  should the function authorize with this cookie
- * @returns {object} {
- *  type: 'success' | 'fail',
- *  // success
- *  response: {
- *    _id: string,
- *    name: string,
- *    username: string,
- *    email: string,
- *  }
- *  // fail
- *  errors: [{
- *    msg: string,  // error
- *    param: string // field
- *  }, ...]
- * }
+ * @returns {Response}
  */
 export function authorize(url = '/api/user/auth', cookie) {
   return (dispatch) => {
@@ -99,21 +107,7 @@ export const { loginStart, loginSuccess, loginFailure } = authSlice.actions;
  * @param {string}  password   password
  * @param {boolean} keepCookie should the function return the authentication cookie along
  *                              with the response, defaults to false
- * @returns {object} {
- *  type: 'success' | 'fail',
- *  // success
- *  response: {
- *    _id: string,
- *    name: string,
- *    username: string,
- *    email: string,
- *  }
- *  // fail
- *  errors: [{
- *    msg: string,  // error
- *    param: string // field
- *  }, ...]
- * }
+ * @returns {Response}
  */
 export function login({
   username, password, keepCookie = false, url = '/api/user/login',
@@ -137,14 +131,7 @@ export function login({
 export const { logoutStart, logoutSuccess, logoutFailure } = authSlice.actions;
 /**
  * Logout of kashoot
- * @returns {object} {
- *  type: 'success' | 'fail',
- *  // fail
- *  errors: [{
- *    msg: string,  // error
- *    param: string // field
- *  }, ...]
- * }
+ * @returns {Response}
  */
 export function logout(url = '/api/user/logout', cookie) {
   const options = {
